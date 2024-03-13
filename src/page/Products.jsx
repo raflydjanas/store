d5d8846b5560cc058edccaf91a631d2563622aa1
raflydjanas <moh.raflidjans@gmail.com>
2024-03-13 16:58:56 +0800

    when no data show  alert in window

--- src/page/Products.jsx
+++ src/page/Products.jsx
@@ -6,43 +6,38 @@ import { useContext, useEffect, useState } from "react";
 import { DarkMode } from "../constext/DarkMode";
 
 const ProductsPage = () => {
-    const { isDarkMode } = useContext(DarkMode)
-    const [products, setProducts] = useState([]);
-    const cart = useSelector(state => state.cart.data);
+  const { isDarkMode } = useContext(DarkMode);
+  const [products, setProducts] = useState([]);
+  const cart = useSelector((state) => state.cart.data);
 
-    useEffect(() => {
-        getProducts().then(res => {
-            setProducts(res.data);
-        });
+  useEffect(() => {
+    getProducts().then((res) => {
+      setProducts(res.data);
     });
+  });
 
-    useEffect(() => {
-       if(products.length > 0 && cart.length > 0) {
-           localStorage.setItem('cart', JSON.stringify(cart))
-       }
-    }, [cart, products])
+  useEffect(() => {
+    if (products.length > 0 && cart.length > 0) {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart, products]);
 
-    return (
-       <>
-         <NavBar />
-         <div className={` flex justify-center items-center ${isDarkMode && 'bg-slate-900'}`}>
-            <div className="flex flex-wrap justify-center p-6 md:w-full">
-                {products.map((product) => (
-                    <CartProducts key={product.id}>
-                        <CartProducts.Header image={product.image} id={product.id}/>
-                        <CartProducts.Body title={product.title}>
-                            {product.description}
-                        </CartProducts.Body>
-                        <CartProducts.Footer 
-                            price={product.price} 
-                            id={product.id}
-                            />
-                    </CartProducts>
-                ))}
-            </div>
+  return (
+    <>
+      <NavBar />
+      <div className={` flex justify-center items-center ${isDarkMode && "bg-slate-900"}`}>
+        <div className="flex flex-wrap justify-center p-6 md:w-full">
+          {products.map((product) => (
+            <CartProducts key={product.id}>
+              <CartProducts.Header image={product.image} id={product.id} />
+              <CartProducts.Body title={product.title}>{product.description}</CartProducts.Body>
+              <CartProducts.Footer price={product.price} id={product.id} />
+            </CartProducts>
+          ))}
         </div>
-       </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default ProductsPage;
